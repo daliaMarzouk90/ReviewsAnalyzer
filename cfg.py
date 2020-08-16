e9af2b090faa68d8ca8a174dc870c0422793ceab
daliaMarzouk90 <69721475+daliaMarzouk90@users.noreply.github.com>
2020-08-16 09:22:23 +0200

    Update cfg.py

--- cfg.py
+++ cfg.py
@@ -4,9 +4,9 @@ Created on Tue Aug 11 12:50:44 2020
 
 @author: Dalia
 """
-apikey = "dOdfn1-UUG38bEv_1_sUhnLRMZcMViZCMpabvAL5BVEZ"
+apikey = "00000000000"
 version = '2017-09-21'
-url= "https://api.eu-gb.tone-analyzer.watson.cloud.ibm.com/instances/67b2aded-5171-4b0c-afba-600fb8cfa3bb"
+url= "https://api.eu-gb.tone-analyzer.watson.cloud.ibm.com/instances/000000"
 esHost = "localhost"
 esPortNo = 9200
 indexName = "hotels"
